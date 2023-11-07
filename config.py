2e5ef7ab4819ad1664bef659fd217b90e9aff7ec
harry <harrisongitau3@gmail.com>
2023-11-07 14:55:46 +0300

    Dev: More features
    - Error handling
    - User auth
    - Update UI of the base template
    - Update UI of login and register templates

--- config.py
+++ config.py
@@ -6,14 +6,7 @@ class Config(object):
     """Define app configurations"""
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard-to-guess'
 
-<<<<<<< HEAD
-    # Database migration
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-=======
-    # Data configuration
+    # Database
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.dib')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
->>>>>>> 6336f05e94c025f568b88c28e2d8c75b446acfcd
